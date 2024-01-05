e9f0e551b6c27e56887fb104e63a2b3050fe66d4
PhenixWen <365401216@qq.com>
2024-01-05 17:29:56 +0800

    Update diy.sh

--- devices/ramips_mt7620/diy.sh
+++ devices/ramips_mt7620/diy.sh
@@ -4,6 +4,11 @@ shopt -s extglob
 
 sh -c "curl -sfL https://github.com/openwrt/openwrt/commit/2e6d19ee32399e37c7545aefc57d41541a406d55.patch | patch -d './' -p1 --forward" || true
 
+sed -i "/helloworld/d" "feeds.conf.default"
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 
 
